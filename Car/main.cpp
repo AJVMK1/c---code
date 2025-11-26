3d505aa99f5427deb7ceefdf157aa649a8be98ec
AJVMk1 <a.veratony@gmail.com>
2025-11-26 00:45:15 -0600

    added functions

--- Car/main.cpp
+++ Car/main.cpp
@@ -4,16 +4,38 @@ using namespace std;
 
 #include "car.cpp"
 
+void PrintMake(Car cars[], int size);
+void SetCarInfo(string make, string model, int year, Car cars[], int size);
+
 int main() {
 
-    Car car("Tesla", "Model S", 2022);
-    cout << "My car is a: " << endl;
-    cout << car.getMake() << " " << car.getModel() << " " << car.getYear() << endl;
-    car.setMake("Toyota");
-    car.setModel("Corolla");
-    car.setYear(2020);
-    cout << "\nMy car has changed to a: " << endl;
-    cout << car.getMake() << " " << car.getModel() << " " << car.getYear() << endl;
+ Car cars[3] = {
+        Car("Tesla", "Model S", 2022),
+        Car("Ford", "Mustang", 2018),
+        Car("Honda", "Civic", 2019)
+    };
+
+    cout << "Original Makes:" << endl;
+    PrintMake(cars, 3);
+
+    SetCarInfo("Toyota", "Corolla", 2020, cars, 3);
+
+    cout << "New Makes: " << endl;
+    PrintMake(cars, 3);
 
     return 0;
+}
+
+void PrintMake(Car cars[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << cars[i].getMake() << endl;
+    }
+}
+
+void SetCarInfo(string make, string model, int year, Car cars[], int size) {
+    for (int i = 0; i < size; i++) {
+        cars[i].setMake(make);
+        cars[i].setModel(model);
+        cars[i].setYear(year);
+    }
 }
